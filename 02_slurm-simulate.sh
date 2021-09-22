73fa3f0d24bee76d3e6a42dc167a8fe202036984
Ian Caldas <ivc2@cornell.edu>
2021-09-22 15:55:04 -0400

    Make simulations run better on the cluster.

--- 02_slurm-simulate.sh
+++ 02_slurm-simulate.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --mem=18000
 #SBATCH --account=bscb02
-#SBATCH --output=/home/ivc/slurm-outputs/simulate-%j.out
+#SBATCH --output="/home/ivc2/slurm-outputs/%x-%j.out"
 #SBATCH --partition=regular,long7,long30
 
 echo "Workstation is ${HOSTNAME}, partition is ${SLURM_JOB_PARTITION}."
@@ -32,8 +32,7 @@ conda activate python39
 
 echo "Running simulations."
 
-snakemake -c1 --use-conda --snakefile 02_simulate.smk "$@" --config slim=bin/slim3.6
-normalization_stats=resources/normalization-stats.tsv simulations=2 --conda-prefix ~/drosophila-sweeps-conda
+snakemake -c1 --use-conda --snakefile 02_simulate.smk "$@" --config slim=bin/slim3.6 normalization_stats=resources/normalization-stats.tsv --conda-prefix ~/drosophila-sweeps-conda
 
 echo "Deactivating conda."
 conda deactivate
--- 02_submit-simulation-jobs.sh
+++ 02_submit-simulation-jobs.sh
@@ -4,8 +4,7 @@ NUM_SLURM_JOBS=2
 for file in $(ls 02_configs/*.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    COMMAND="sbatch --array=1-${NUM_SLURM_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}"
-    echo ${COMMAND}
+    sbatch --array=1-${NUM_SLURM_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
 done
 
 
