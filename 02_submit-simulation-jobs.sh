f71b9b249c859bbfb671cf933798ca7e8f1cf8d4
Ian Caldas <ivc2@cornell.edu>
2021-09-28 12:27:57 -0400

    Restructure directory structure for simulation configs. Set final slurm job parameters.

--- 02_submit-simulation-jobs.sh
+++ 02_submit-simulation-jobs.sh
@@ -1,10 +1,16 @@
+NUM_TRAINING_JOBS=1100
+NUM_ROBUSTNESS_JOBS=200
 
-NUM_SLURM_JOBS=2
-
-for file in $(ls 02_configs/*.yaml)
+for file in $(ls 02_configs/training/*.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    sbatch --array=1-${NUM_SLURM_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+    sbatch --array=1-${NUM_TRAINING_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+done
+
+for file in $(ls 02_configs/robustness/*.yaml)
+do
+    NAME=$(basename ${file} .yaml)
+    sbatch --array=1-${NUM_ROBUSTNESS_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
 done
 
 
