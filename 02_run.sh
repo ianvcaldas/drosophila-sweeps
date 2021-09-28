febf68d1995eec9cbfe8c7db07f6490f9e22f1fe
Ian Caldas <ivc2@cornell.edu>
2021-09-28 12:24:56 -0400

    Fix bug where frequency_at_sampling and frequency_at_selection showed up as two parameters each. Renamed one of them to actual_frequency_at_sampling or at_selection.

--- 02_run.sh
+++ 02_run.sh
@@ -1,7 +1,11 @@
 
-CONFIGFILE=02_configs/test/testing-hard.yaml
+CONFIGFILES=$(ls 02_configs/test/*.yaml)
 
-snakemake -c4 --use-conda --snakefile 02_simulate.smk --configfile ${CONFIGFILE} "$@" --config slim=bin/slim3.6 normalization_stats=resources/normalization-stats.tsv simulations=3 use_subdirectory=true
+for file in ${CONFIGFILES}
+do
+    snakemake -c4 --use-conda --snakefile 02_simulate.smk --configfile ${file} "$@" --config slim=bin/slim3.6 normalization_stats=resources/normalization-stats.tsv simulations=3 use_subdirectory=true
+done
+
 
 # To re-run all the rules that depend on a script or notebook after it's changed, run
 # snakemake --forcerun [rule with script or notebook].
