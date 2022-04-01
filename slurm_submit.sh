57e1acc6ef9c2f05e99846e9b29a20fb792a6c61
Ian Caldas <ivc2@cornell.edu>
2022-04-01 13:54:17 -0400

    Correct script name

--- slurm_submit.sh
+++ slurm_submit.sh
@@ -9,7 +9,7 @@ CONFIG=simulation-parameters/main-fixedsweeps-neutral.yaml
 NAME="neutral"
 NUM_JOBS=100
 
-sbatch --array=1-${NUM_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${CONFIG}
+sbatch --array=1-${NUM_JOBS} -J ${NAME} slurm_simulate.sh --configfile ${CONFIG}
 
 
 # Training data and data for checking robustness
@@ -20,13 +20,13 @@ NUM_ROBUSTNESS_JOBS=200
 for file in $(ls simulation-parameters/training/*.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    sbatch --array=1-${NUM_TRAINING_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+    sbatch --array=1-${NUM_TRAINING_JOBS} -J ${NAME} slurm_simulate.sh --configfile ${file}
 done
 
 for file in $(ls simulation-parameters/robustness/*.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    sbatch --array=1-${NUM_ROBUSTNESS_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+    sbatch --array=1-${NUM_ROBUSTNESS_JOBS} -J ${NAME} slurm_simulate.sh --configfile ${file}
 done
 
 
@@ -41,26 +41,26 @@ NUM_EXTRA_ROBUSTNESS_SGV=100
 for file in $(ls simulation-parameters/training/*sgv.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    sbatch --array=1-${NUM_EXTRA_TRAINING_SGV} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+    sbatch --array=1-${NUM_EXTRA_TRAINING_SGV} -J ${NAME} slurm_simulate.sh --configfile ${file}
 done
 
 for file in $(ls simulation-parameters/training/*rnm.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    sbatch --array=1-${NUM_EXTRA_TRAINING_RNM} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+    sbatch --array=1-${NUM_EXTRA_TRAINING_RNM} -J ${NAME} slurm_simulate.sh --configfile ${file}
 done
 
 
 for file in $(ls simulation-parameters/robustness/*sgv.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    sbatch --array=1-${NUM_EXTRA_ROBUSTNESS_SGV} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+    sbatch --array=1-${NUM_EXTRA_ROBUSTNESS_SGV} -J ${NAME} slurm_simulate.sh --configfile ${file}
 done
 
 for file in $(ls simulation-parameters/robustness/*rnm.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    sbatch --array=1-${NUM_EXTRA_ROBUSTNESS_RNM} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+    sbatch --array=1-${NUM_EXTRA_ROBUSTNESS_RNM} -J ${NAME} slurm_simulate.sh --configfile ${file}
 done
 
 
@@ -71,5 +71,5 @@ NUM_HARD_JOBS=20
 for file in $(ls simulation-parameters/hard-sweeps-with-known-s/*.yaml)
 do
     NAME=$(basename ${file} .yaml)
-    sbatch --array=1-${NUM_HARD_JOBS} -J ${NAME} 02_slurm-simulate.sh --configfile ${file}
+    sbatch --array=1-${NUM_HARD_JOBS} -J ${NAME} slurm_simulate.sh --configfile ${file}
 done
